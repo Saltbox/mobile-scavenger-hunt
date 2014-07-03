5383f7ec1c59ef348023f9dcae311df30f061375
danny pham <pham.danny.t@gmail.com>
2014-07-03 12:03:02 -0700

    moment js

--- static/js/main.js
+++ static/js/main.js
@@ -62,15 +62,17 @@ $(document).ready(function() {
     }
   };
 
+  // hide participant email error message when typing
   $("#participants-template").keypress(function() {
     $('#participant-error').hide();
   });
 
-  // add item to table and list for later submission
+  // add item to table for later submission via button
   $("#add-item").on("click", (function() {
       addInput('items');
   }));
 
+  // add item to table for later submission via "enter" on keyboard
   $('input#items-template').keydown(function(event) {
     if (event.keyCode == 13) {
       event.preventDefault();
@@ -78,11 +80,12 @@ $(document).ready(function() {
     }
   });
 
-  // add participant to table and list for later submission
+  // add participant to list for later submission via button
   $("#add-participant").on("click", (function() {
       addInput('participants');
   }));
 
+  // add participant to list for later submission via "enter" on keyboard
   $('input#participants-template').keydown(function(event) {
     if (event.keyCode == 13) {
       event.preventDefault();
@@ -90,10 +93,12 @@ $(document).ready(function() {
     }
   });
 
+  // toggle for whether or not an item is required
   $("td.requireditem :checkbox").change(function(t, e) {
     console.log('t e', t, e);
   });
 
+  // toggle default/custom welcome message
   $('input[name=messagetoggle]').change(function(event) {
     if (event.target.value == 'default') {
       $('textarea[name=welcome_message]').val(
@@ -105,6 +110,7 @@ $(document).ready(function() {
     }
   });
 
+  // toggle default/custom congratulations message
   $('input[name=congratulationstoggle]').change(function(event) {
     if (event.target.value == 'default') {
       $('textarea[name=congratulations_message]').val(
@@ -114,5 +120,11 @@ $(document).ready(function() {
     else {
       $('textarea[name=congratulations_message]').val('');
     }
+  });
+
+  $('.uglytime').each(function(index, e) {
+    var currentTime = $(e).html();
+    var prettyTime = moment(currentTime).format("MM-DD-YYYY");
+    $(e).text(prettyTime);
   });
 });
