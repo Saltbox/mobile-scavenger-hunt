e4aea7cbc6feec3acab64eeb7eb6534ce69b3fb6
danny pham <pham.danny.t@gmail.com>
2014-07-08 12:20:59 -0700

    toggling required items func

--- static/js/main.js
+++ static/js/main.js
@@ -4,40 +4,68 @@ $(document).ready(function() {
   var itemCount = 0;  // no. maybe make a class or something.
   var participantCount = 0;
 
+  // toggle if all items are required for success
+  var allItemsRequired = $("input[name=all_required]").val();
+  $("input[name=all_required]").change(function() {
+    allItemsRequired = $("input[name=all_required]").val();
+    console.log(allItemsRequired);
+    if (allItemsRequired) {
+      $('input.hunt-items').prop('checked', true);
+    }
+  });
+
+  // toggle for whether or not an item is required
+  // parent needed to help detect dynamically added content
+  var $parent = $('#items-group .table.table-condensed');
+  $($parent).on("change", ".hunt-items", function(e) {
+    // if value is in target, it's been set to "on". toggling removes value.
+    if ('value' in e.currentTarget) {
+      $($('input[name=all_required]')[1]).prop('checked', true);
+    }
+  });
+
+  // create input name for item required that works with wtforms
   var itemRequiredName = function(index) {
     return "items-" + index + "-required";
   };
 
+  // make new item checkbox
   var tdCheckbox = function(index) {
-    // return "<td><input type='checkbox' id='item" + index + "' onclick='toggleRequired(" + index + ")'></td>";
+    if (allItemsRequired) {
+      return "<td><input type='checkbox' checked='on' class='hunt-items' name='" + itemRequiredName(index) + "'></td>";
+    }
     return "<td><input type='checkbox' class='hunt-items' name='" + itemRequiredName(index) + "'></td>";
-    // return "<td><input type='checkbox' id='item" + index +"'></td>";
   };
 
+  // since jquery wrap doesn't seem to work
   var tdItem = function(item_name) {
-    return "<td>" + item_name + "</td>";
+    return "<td class='item-required'>" + item_name + "</td>";
   };
 
+  // create input name property that works with wtforms
   var itemName = function(itemCount) {
     return 'items-' + itemCount + '-name';
   };
 
+  // add tr to item table
   var addItemRow = function(itemCount, fieldValue) {
-    $('#item-tbody').append(
-      "<tr>" + tdCheckbox(itemCount) + tdItem(fieldValue) + "</tr>"
-    );
+    var row = "<tr>" + tdCheckbox(itemCount) + tdItem(fieldValue) + "</tr>";
+    $('#item-tbody').append(row);
   };
 
+  // helper for addInput that displays each added participant email
   var addParticipantLi = function(fieldValue) {
     $('#participant-emails').append('<li>' + fieldValue + '</li>');
   };
 
+  // helper for addInput
   var addNewField = function(fieldType, name, fieldValue) {
     var newField = $('<input type="hidden">').attr('name', name)
                                              .attr('value', fieldValue);
     $('#' + fieldType + '-group .input-group').append(newField);
   };
 
+  // add item or participant hidden input field that works with wtforms
   var addInput = function(fieldType, count) {
     var fieldName = itemName(count);
     var fieldInput = $('input#' + fieldType + '-template');
@@ -99,14 +127,7 @@ $(document).ready(function() {
     }
   });
 
-  // toggle for whether or not an item is required
-  // parent needed to help detect dynamically added content
-  var $parent = $('#items-group .table.table-condensed');
-  $($parent).on("change", ".hunt-items", function(e) {
-    console.log('e', e);
-    console.log($(e).parent().index());
-  });
-
+  // helper function for message and congratulations toggling
   var toggleTextarea = function(textareaSelector, defaultMsg) {
     if ($(textareaSelector).is(':hidden')) {
       $(textareaSelector).show(500);
@@ -135,6 +156,7 @@ $(document).ready(function() {
     );
   });
 
+  // time formatter
   $('.uglytime').each(function(index, e) {
     var currentTime = $(e).html();
     var prettyTime = moment(currentTime).format("MM-DD-YYYY");
