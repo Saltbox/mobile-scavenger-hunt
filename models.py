fa18c62f309b0a58cde945ba12d05dcf4bcc9e96
danny pham <pham.danny.t@gmail.com>
2014-08-12 14:43:16 -0700

    modified admin model. no names

--- models.py
+++ models.py
@@ -6,10 +6,8 @@ import sys
 class Admin(db.Model):
     __tablename__ = 'admins'
     admin_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
     email = db.Column(db.String(320), unique=True)
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(64))
     hunts = db.relationship('Hunt', backref='hunts')
 
     def __repr__(self):
